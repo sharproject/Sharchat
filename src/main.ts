bdf91045ec7726bcbd6563b72726cbc1767d21f0
code-ga <tritranduc11@gmail.com>
2023-01-23 09:35:32 +0700

    add doc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './util/http-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // setup class Validation
     app.useGlobalPipes(new ValidationPipe());
+
+    // setup cors
     app.enableCors();
+
+    const DocumentConfig = new DocumentBuilder()
+        .setTitle('Chat app Server doc')
+        .setDescription('document')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, DocumentConfig);
+    SwaggerModule.setup('docs', app, document);
+
+    // error handling
+    app.useGlobalFilters(new HttpExceptionFilter());
+
     const PORT = Number(process.env.PORT || 8080);
     await app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
